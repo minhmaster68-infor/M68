f86e07403447f744f157f1d33c257fdb11a26d0f
minhmaster68-infor <minhmaster68@gmail.com>
2026-08-18 09:16:25 +0700

    Add files via upload

---
+++ Testb/Node.js
@@ -0,0 +1,26 @@
+const gameQuizzes = [
+  {
+    type: "english_cloze",
+    category: "English",
+    question: "Our atmosphere mainly consists _____ nitrogen, oxygen, argon, and carbon dioxide.",
+    options: ["up", "to", "of", "in"],
+    answer: "of",
+    reward: 50 // Nước tưới cây
+  },
+  {
+    type: "science_calc",
+    category: "Science",
+    question: "Camera ghi nhận ô tô đi 10m trong 0.5 giây. Tốc độ của xe là bao nhiêu km/h?",
+    options: ["36 km/h", "50 km/h", "72 km/h", "100 km/h"],
+    answer: "72 km/h",
+    reward: 100 // Phân bón
+  },
+  {
+    type: "english_grammar",
+    category: "English",
+    question: "The teacher got to school _____ time so that he could have coffee before class.",
+    options: ["on", "in", "at", "for"],
+    answer: "in",
+    reward: 50
+  }
+];
